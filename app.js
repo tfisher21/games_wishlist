d6264c5ae11a50372ee250d353d0cd20e05c5fb9
Tyler <tyler.r.fisher@icloud.com>
2018-06-21 01:16:38 +0000

    Add button to results view to submit to database

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require("express"),
     app     = express(),
     request = require("request"),
+    bodyParser = require("body-parser"),
     mongoose = require("mongoose");
 
 require("dotenv").load();
 mongoose.connect("mongodb://localhost/crm");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 
@@ -12,10 +14,12 @@ app.set('view engine', 'ejs');
 // ROUTES //
 ////////////
 
+// LANDING
 app.get("/", function(req, res){
   res.render("landing");
 });
 
+// SEARCH RESULTS
 app.get("/search", function(req, res){
     var query = req.query.query;
     var url = "https://www.giantbomb.com/api/releases/?api_key=" + process.env.gbapi + "&format=JSON&filter=region:1,platform:157,name:" + query;
@@ -26,6 +30,11 @@ app.get("/search", function(req, res){
             res.render("results", {data:data});
         }
     });
+});
+
+// CREATE - Add selected title to wishlist
+app.post("/wishlist", function(req, res){
+    
 });
 
 //////////////////////////////
