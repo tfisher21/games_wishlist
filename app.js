eb676d68065e5095aeba528540f3d80df64bd2f1
Tyler <tyler.r.fisher@icloud.com>
2018-06-21 03:07:04 +0000

    Create ability to add entries to the wishlist database and show in the index view

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express"),
     app     = express(),
     request = require("request"),
     bodyParser = require("body-parser"),
+    Wishlist = require("./models/wishlist"),
     mongoose = require("mongoose");
 
 require("dotenv").load();
@@ -9,20 +10,28 @@ mongoose.connect("mongodb://localhost/crm");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+// GLOBAL VARIABLES
+var apiString = "?api_key=" + process.env.gbapi + "&format=JSON";
 
 ////////////
 // ROUTES //
 ////////////
 
-// LANDING
+// INDEX - LANDING
 app.get("/", function(req, res){
-  res.render("landing");
+  Wishlist.find({}, function(err, allWishlists){
+      if (err) {
+          console.log(err);
+      } else {
+          res.render("landing", {wishlist: allWishlists});
+      }
+  });
 });
 
 // SEARCH RESULTS
 app.get("/search", function(req, res){
     var query = req.query.query;
-    var url = "https://www.giantbomb.com/api/releases/?api_key=" + process.env.gbapi + "&format=JSON&filter=region:1,platform:157,name:" + query;
+    var url = "https://www.giantbomb.com/api/releases/" + apiString + "&filter=region:1,platform:157,name:" + query;
     
     request(url, function(error, response, body){
         if (!error && response.statusCode == 200) {
@@ -34,7 +43,26 @@ app.get("/search", function(req, res){
 
 // CREATE - Add selected title to wishlist
 app.post("/wishlist", function(req, res){
+    var url = req.body.gameAPIurl + apiString;
     
+    request(url, function(error, response, body){
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            
+            var name = data.results.name;
+            var gbLink = data.results.site_detail_url;
+            var deck = data.results.deck;
+            var newWishlist = {name: name, gbLink: gbLink, deck: deck};
+            
+            Wishlist.create(newWishlist, function(err, newlyCreated){
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.redirect("/");
+                }
+            });
+        }
+    });
 });
 
 //////////////////////////////
